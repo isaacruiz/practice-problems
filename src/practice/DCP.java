c6447597ab3e56c971c0812c3f379012551f6688
Isaac <isaacmruiz@gmail.com>
2019-05-13 12:28:13 -0500

    Started problem 99

--- src/practice/DCP.java
+++ src/practice/DCP.java
@@ -12,11 +12,19 @@ public class DCP {
     *Given a list of numbers and k, return whether any two numbers from list
     *add up to k
     */
-    public static boolean Problem1(int[] nums, int k){
+    public boolean Problem1(int[] nums, int k){
         for(int i = 0; i < nums.length; i++)
             for(int j = i + 1; j < nums.length; j++)
                 if(nums[i] + nums[j] == k)
                     return true;
+        return false;
+    }
+    
+    //Given a 2D board of characters and a word, find if the word exists in the
+    //grid. The word can be constructed from letters of a sequentially vertically
+    //and horizontally adjacent cells. The same cell can not be used twice
+    public boolean Problem99(int[][] board, String word){
+        
         return false;
     }
 }
