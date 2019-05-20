0558f1abc095d618c0f5ccebc4b7592ad86bd5bf
Isaac <isaacmruiz@gmail.com>
2019-05-20 00:38:05 -0500

    Added unit testing code

---
+++ test/practice/Problems/DCPTest.java
@@ -0,0 +1,164 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practice.Problems;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import practice.DCP;
+
+/**
+ *
+ * @author Isaac
+ */
+public class DCPTest {
+    DCP dcp;
+    public DCPTest() {
+        dcp = new DCP();
+    }
+    /**
+     * Test of Problem1 method, of class DCP.
+     */
+    @Test
+    public void testProblem1() {
+        System.out.println("Problem1A");
+        int[] nums1 = {1,2,3,4};
+        int[] nums2 = {1,0,0,6};
+        int[] nums3 = {5};
+        assertEquals(dcp.Problem1A(nums1, 5), true);
+        assertEquals(dcp.Problem1B(nums1, 5), true);
+        assertEquals(dcp.Problem1A(nums2, 7), true);
+        assertEquals(dcp.Problem1B(nums2, 7), true);
+        assertEquals(dcp.Problem1A(nums3, 5), false);
+        assertEquals(dcp.Problem1B(nums3, 5), false);
+    }
+
+    /**
+     * Test of Problem2 method, of class DCP.
+     */
+    @Test
+    public void testProblem2() {
+        System.out.println("Problem2A");
+        int[] nums1 = {1, 2, 3, 4, 5};
+        int[] res1 = {120, 60, 40, 30, 24};
+        int[] nums2 = {5, 6, 7, 9};
+        int[] res2 = {378, 315, 270, 210};
+        int[] nums3 = {12, 3, 0, 1};
+        int[] res3 = {0, 0, 36, 0};
+        assertArrayEquals(dcp.Problem2A(nums1), res1);
+        assertArrayEquals(dcp.Problem2B(nums1), res1);
+        assertArrayEquals(dcp.Problem2A(nums2), res2);
+        assertArrayEquals(dcp.Problem2B(nums2), res2);
+        assertArrayEquals(dcp.Problem2A(nums3), res3);
+        assertArrayEquals(dcp.Problem2B(nums3), res3);        
+    }
+
+    /**
+     * Test of Problem102 method, of class DCP.
+     */
+    @Test
+    public void testProblem102() {
+        System.out.println("Problem102");
+        ArrayList<Integer> nums = new ArrayList<>();
+        ArrayList<Integer> expected = new ArrayList<>();
+        nums.add(1);
+        nums.add(2);
+        nums.add(3);
+        nums.add(4);
+        nums.add(5);
+        int k = 3;
+        expected.add(1);
+        expected.add(2);
+        assertEquals(dcp.Problem102(nums, k), expected);
+        expected.clear();
+        expected.add(3);
+        expected.add(4);
+        expected.add(5);
+        assertEquals(dcp.Problem102(nums, 12), expected);
+        nums.clear();
+        expected.clear();
+        nums.add(0);
+        nums.add(0);
+        nums.add(0);
+        nums.add(2);
+        nums.add(2);
+        nums.add(2);
+        expected.add(0);
+        expected.add(0);
+        expected.add(0);
+        expected.add(2);
+        expected.add(2);
+        expected.add(2);
+        assertEquals(dcp.Problem102(nums, 6), expected);
+    }
+
+    /**
+     * Test of Problem103 method, of class DCP.
+     */
+    @Test
+    public void testProblem103() {
+        System.out.println("Problem103");
+        char[] chars = {'e', 'l', 'o'};
+        assertEquals(dcp.Problem103("Hello sir", chars), "ello");
+        
+        chars = new char[]{'o', 'l', 'e'};
+        assertEquals(dcp.Problem103("Hello sir", chars), "ello");
+        
+        chars = new char[]{'H', 'l', 'r'};
+        assertEquals(dcp.Problem103("Hello sir", chars), "Hello sir");
+        
+        chars = new char[]{'r'};
+        assertEquals(dcp.Problem103("Hello sir", chars), "r");
+        
+        chars = new char[]{'e', 'b'};
+        assertEquals(dcp.Problem103("absdieslbde", chars), "bde");
+        assertNull(dcp.Problem103("ice cream", chars));
+    }
+
+    /**
+     * Test of Problem104A method, of class DCP.
+     */
+    @Test
+    public void testProblem104() {
+        System.out.println("Problem104");
+        LinkedList<Character> evenPalindrome = new LinkedList<>();
+        evenPalindrome.add('g');
+        evenPalindrome.add('s');
+        evenPalindrome.add('b');
+        evenPalindrome.add('a');
+        evenPalindrome.add('a');
+        evenPalindrome.add('b');
+        evenPalindrome.add('s');
+        evenPalindrome.add('g');
+        
+        LinkedList<Character> oddPalindrome = new LinkedList<>();
+        oddPalindrome.add('g');
+        oddPalindrome.add('s');
+        oddPalindrome.add('b');
+        oddPalindrome.add('a');
+        oddPalindrome.add('b');
+        oddPalindrome.add('s');
+        oddPalindrome.add('g');
+        
+        assertTrue(dcp.Problem104A(evenPalindrome));
+        assertTrue(dcp.Problem104B(evenPalindrome));
+        assertTrue(dcp.Problem104A(oddPalindrome));
+        assertTrue(dcp.Problem104B(oddPalindrome));
+        
+        oddPalindrome.removeLast();
+        assertFalse(dcp.Problem104A(oddPalindrome));
+        assertFalse(dcp.Problem104B(oddPalindrome));
+        
+        evenPalindrome.removeFirst();
+        assertFalse(dcp.Problem104A(evenPalindrome));
+        assertFalse(dcp.Problem104B(evenPalindrome));
+    } 
+}
