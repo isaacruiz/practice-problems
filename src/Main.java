53df2e1e585108a6e8663cf611652b8b44c14ce0
Isaac <isaacmruiz@gmail.com>
2019-05-22 01:33:46 -0500

    Modified file structure

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        CCI cci = new CCI();
     }
 }
 
---
+++ src/Problems/PracticeProblems.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practice.Problems;
+
+/**
+ *
+ * @author Isaac
+ */
+public class PracticeProblems {
+    /*
+     * Write function that returns list of all permutations of given string
+     */
+    
+    /*
+     * Write function that takes two lists of characters as parameters and
+     * returns two lists of characters. The first list contains characters that
+     * were in the first paramter list that was not in the second and vice versa
+     * for the second. If there are duplicate characters, the list will contain
+     * duplicate characters equivalent to the number of times a character was
+     * present in the list minus the number of times present in the other.
+     */
+	
+    
+    /*
+     * Write a function that takes a string as input and returns the longest
+     * palindrome substring in the input string (Assume that there is only one
+     * longest palindrome
+     */
+    
+    /*
+     * Write a function that takes a string composed of one or more sentences
+     * and a list of commonly used words and returns the most common word or
+     * words used in the string that were not in the list of common words
+     */
+}
